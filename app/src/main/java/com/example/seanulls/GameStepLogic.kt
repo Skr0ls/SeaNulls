705ec90ad9f78d9330e1865b8828ad8faee5c78d
Dmitry <lpdimakot11@gmail.com>
2023-12-19 05:47:35 +0300

    refactor_fst

--- app/src/main/java/com/example/seanulls/GameStepLogic.kt
+++ app/src/main/java/com/example/seanulls/GameStepLogic.kt
@@ -2,12 +2,9 @@ package com.example.seanulls
 
 import android.content.Context
 import android.content.Intent
-import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.SystemClock
-import android.util.Log
 import android.view.View
 import android.widget.Chronometer
 import android.widget.Chronometer.OnChronometerTickListener
@@ -111,7 +108,7 @@ class GameStepLogic {
     }
 
     // Инициализация игрового поля
-    fun initializeGameField(_fieldOne: GridLayout?, _fieldTwo: GridLayout?, _cellSide: Int, _context: Context?){
+    /*fun initializeGameField(_fieldOne: GridLayout?, _fieldTwo: GridLayout?, _cellSide: Int, _context: Context?){
         gridFieldOne = _fieldOne
         gridFieldTwo = _fieldTwo
         cellSide = _cellSide
@@ -139,10 +136,160 @@ class GameStepLogic {
                 fieldTwo[pos!!.x + 1][pos.y + 1] = ship.id
             }
         }
+    }*/
+
+    fun initializeGameField(_fieldOne: GridLayout?, _fieldTwo: GridLayout?, _cellSide: Int, _context: Context?){
+        setupFieldsAndButtons(_fieldOne, _fieldTwo, _cellSide, _context)
+
+        initializeGameFields()
+
+        convertAndSetShips()
     }
 
-    private fun initializeField(player: Int, field: GridLayout?, buttonField: Array<Array<CustomButton?>>): Boolean {
-        for (i in 0..11) for (j in 0..11) buttonField[i][j] = CustomButton(context!!)
+    private fun setupFieldsAndButtons(_fieldOne: GridLayout?, _fieldTwo: GridLayout?, _cellSide: Int, _context: Context?){
+        gridFieldOne = _fieldOne
+        gridFieldTwo = _fieldTwo
+        cellSide = _cellSide
+        context = _context
+
+        buttonFieldOne = Array(12) { arrayOfNulls(12) }
+        buttonFieldTwo = Array(12) { arrayOfNulls(12) }
+
+        layout = AppAssetsManager.layoutSprites
+    }
+
+    private fun initializeGameFields(){
+        initializeField(1, gridFieldOne, buttonFieldOne)
+        initializeField(2, gridFieldTwo, buttonFieldTwo)
+    }
+
+    private fun convertAndSetShips(){
+        shipsOne = convert(ShipPacksStorage.playerOneShips!!)
+        shipsTwo = convert(ShipPacksStorage.playerTwoShips!!)
+
+        for (ship in shipsOne!!) {
+            for (pos in ship.positions) {
+                fieldOne[pos!!.x + 1][pos.y + 1] = ship.id
+            }
+        }
+
+        for (ship in shipsTwo!!) {
+            for (pos in ship.positions) {
+                fieldTwo[pos!!.x + 1][pos.y + 1] = ship.id
+            }
+        }
+    }
+
+    private fun initializeField(player: Int, field: GridLayout?, buttonField: Array<Array<CustomButton?>>): Boolean{
+        // Этап 1: Инициализация переменных и настройка размеров поля
+        initializeButtonField(buttonField)
+        setupFieldSize(field)
+
+        val nullSpace = createNullSpace()
+        field?.addView(nullSpace)
+
+        // Этап 2: Добавление элементов в GridLayout
+        addSymbolsToGridLayout(field)
+
+        // Этап 3: Добавление кнопок и элементов для обоих игроков
+        addButtonsAndSymbolsForPlayers(player, field, buttonField)
+
+        // Возвращается значение true в конце метода
+        return true
+    }
+
+    private fun initializeButtonField(buttonField: Array<Array<CustomButton?>>) {
+        for (i in 0..11) {
+            for (j in 0..11) {
+                buttonField[i][j] = CustomButton(context!!)
+            }
+        }
+    }
+
+    private fun setupFieldSize(field: GridLayout?) {
+        field?.apply {
+            rowCount = 11
+            columnCount = 11
+        }
+    }
+
+    private fun createNullSpace(): ImageView{
+        val nullSpace = ImageView(context)
+        val params = GridLayout.LayoutParams()
+
+        params.width = cellSide
+        params.height = cellSide
+        params.setMargins(0, 0, 0, 0)
+
+        nullSpace.layoutParams = params
+        nullSpace.background = AppAssetsManager.layoutSprites!![0]
+
+        return nullSpace
+    }
+
+    private fun addSymbolsToGridLayout(field: GridLayout?) {
+        field?.apply {
+            for (i in 0..9) {
+                val symbol = ImageView(context)
+                val topParams = GridLayout.LayoutParams()
+
+                topParams.width = cellSide
+                topParams.height = cellSide
+                topParams.setMargins(0, 0, 0, 0)
+
+                symbol.background = layout!![i + 1]
+                symbol.layoutParams = topParams
+
+                addView(symbol)
+            }
+        }
+    }
+
+    private fun addButtonsAndSymbolsForPlayers(player: Int, field: GridLayout?, buttonField: Array<Array<CustomButton?>>) {
+        field?.apply {
+            for (i in 0..9) {
+                val symbol = ImageView(context)
+                val leftParams = GridLayout.LayoutParams()
+
+                leftParams.width = cellSide
+                leftParams.height = cellSide
+                leftParams.setMargins(0, 0, 0, 0)
+
+                symbol.background = layout!![i + 10 + 1]
+                symbol.layoutParams = leftParams
+
+                addView(symbol)
+                buttonField[i][0] = CustomButton(context!!)
+
+                for (j in 0 until 10) {
+                    // Остальные операции по добавлению кнопок и слушателей оставлены здесь
+                    // для добавления кнопок и слушателей для игроков
+                    buttonField[i + 1][j + 1] = CustomButton(context!!)
+                    val gridParams = GridLayout.LayoutParams()
+
+                    gridParams.width = cellSide
+                    gridParams.height = cellSide
+                    gridParams.setMargins(0, 0, 0, 0)
+
+                    buttonField[i + 1][j + 1]!!.id = 100 * player + i * 10 + j
+                    buttonField[i + 1][j + 1]!!.layoutParams = gridParams
+                    buttonField[i + 1][j + 1]!!.background = layout!![21]
+
+                    // Установка слушателей для разных игроков
+                    if (player == 1) buttonField[i + 1][j + 1]!!.setOnClickListener(firstFieldListener)
+                    else if (player == 2) buttonField[i + 1][j + 1]!!.setOnClickListener(secondFieldListener)
+
+                    field.addView(buttonField[i + 1][j + 1])
+                }
+                buttonField[i][11] = CustomButton(context!!)
+            }
+        }
+    }
+
+
+    /*private fun initializeField(player: Int, field: GridLayout?, buttonField: Array<Array<CustomButton?>>): Boolean {
+        for (i in 0..11)
+            for (j in 0..11) buttonField[i][j] = CustomButton(context!!)
         field!!.rowCount = 11
         field.columnCount = 11
 
@@ -160,11 +307,14 @@ class GameStepLogic {
         for (i in 0..9) {
             val symbol = ImageView(context)
             val topParams = GridLayout.LayoutParams()
+
             topParams.width = cellSide
             topParams.height = cellSide
             topParams.setMargins(0, 0, 0, 0)
+
             symbol.background = layout!![i + 1]
             symbol.layoutParams = topParams
+
             field.addView(symbol)
         }
 
@@ -202,7 +352,7 @@ class GameStepLogic {
             buttonField[i][11] = CustomButton(context!!)
         }
         return true
-    }
+    }*/
 
     //Отоброажает разрушения корабля на игровом поле
     fun reflector(ship: PlayableShip, buttonField: Array<Array<CustomButton?>>, field: Array<IntArray>): Boolean {
@@ -254,18 +404,18 @@ class GameStepLogic {
     private fun allButtonsMatchCriteria(): Boolean {
         for (i in 1 until fieldOne.size - 1) {
             for (j in 1 until fieldOne[i].size - 1) {
-                val button = buttonFieldOne[i][j]
-                val buttonBackgroundColor = (button?.background as? ColorDrawable)?.color ?: Color.TRANSPARENT
+                val buttonOne = buttonFieldOne[i][j]
+                val buttonTwo = buttonFieldTwo[i][j]
+                //Проверяем ассеты кнопки
+                if (!checkAssets(buttonOne!!)) return false
 
-                // Проверяем соответствие цвета фона или других ассетов, включая убитые корабли
-                if (buttonBackgroundColor != hitColor && button?.background != layout[22] && !checkAssets(button!!)) {
-                    return false
-                }
+                if(!checkAssets(buttonTwo!!)) return false
             }
         }
         return true
     }
 
+    //Метод для проверки ассетов кнопки
     private fun checkAssets(button: CustomButton): Boolean{
         val buttonBackground = button.background
 
@@ -273,8 +423,63 @@ class GameStepLogic {
             it.equals(AppAssetsManager.horizontalShipAssets.getBody(ShipAssetSample.shipPartType.destroyed)) ||
                     it.equals(AppAssetsManager.horizontalShipAssets.getFront(ShipAssetSample.shipPartType.destroyed)) ||
                     it.equals(AppAssetsManager.verticalShipAssets.getBody(ShipAssetSample.shipPartType.destroyed)) ||
-                    it.equals(AppAssetsManager.verticalShipAssets.getFront(ShipAssetSample.shipPartType.destroyed))
+                    it.equals(AppAssetsManager.verticalShipAssets.getFront(ShipAssetSample.shipPartType.destroyed)) ||
+                    it.equals(hitColor) ||
+                    it.equals(layout[22])
         } ?: false
+    }
+
+    private fun handleButtonClick(isFirstPlayer: Boolean, x: Int, y :Int) {
+        val buttonField = if (isFirstPlayer) buttonFieldOne else buttonFieldTwo
+        val field = if (isFirstPlayer) fieldOne else fieldTwo
+        val ships = if (isFirstPlayer) shipsOne else shipsTwo
+        val textPlayerStep = if (isFirstPlayer) textFirstPlayerStep else textSecondPlayerStep
+
+        val currentButton = buttonField[x + 1][y + 1]
+
+        if (!currentButton!!.isButtonPressed) {
+            if (!currentButton!!.isButtonPressed) {
+                var currentShip: PlayableShip? = null
+
+                for (ship in ships!!) {
+                    if (ship.id == field[x + 1][y + 1]) {
+                        currentShip = ship
+                        break
+                    }
+                }
+                if (currentShip != null) {
+                    currentButton.setPressedStatus()
+                    currentButton.setBackgroundColor(hitColor)
+                    currentShip.incrementDestroyedParts()
+                    field[x + 1][y + 1] = -1
+
+                    if (currentShip.isDestroyed) {
+                        reflector(
+                            currentShip,
+                            if (isFirstPlayer) buttonFieldOne else buttonFieldTwo,
+                            if (isFirstPlayer) fieldOne else fieldTwo
+                        )
+                    }
+                    updateStep()
+
+                    var n = 0
+                    for (ship in ships) {
+                        if (ship.isDestroyed) n++
+                    }
+
+                    if (n == 10) handleGameEnd(if (isFirstPlayer) 2 else 1)
+                }
+
+                if (currentShip == null) {
+                    currentButton.setPressedStatus()
+                    currentButton.background = layout!![22]
+                    field[x + 1][y + 1] = -1
+                    changeStep()
+
+                    if (allButtonsMatchCriteria()) handleGameEnd(if (isFirstPlayer) 2 else 1)
+                }
+            }
+        }
     }
 
     private val onTickListener = OnChronometerTickListener { chronometer ->
@@ -283,11 +488,13 @@ class GameStepLogic {
             if (isFirstPlayerStep) {
                 textFirstPlayerStep!!.visibility = View.INVISIBLE
                 textSecondPlayerStep!!.visibility = View.VISIBLE
+
                 changeStep()
                 chronometer.text = "" + seconds
             } else {
                 textFirstPlayerStep!!.visibility = View.VISIBLE
                 textSecondPlayerStep!!.visibility = View.INVISIBLE
+
                 changeStep()
                 chronometer.text = "" + seconds
             }
@@ -296,92 +503,15 @@ class GameStepLogic {
 
     // Обработка нажатия на кнопку игрового поля первого игрока
     private val firstFieldListener = View.OnClickListener { v ->
-        val button = v as CustomButton
-
-        if (!button.isButtonPressed) {
-            val x = v.getId().toString()[1].code - 48
-            val y = v.getId().toString()[2].code - 48
-            var currentShip: PlayableShip? = null
-
-            for (ship in shipsOne!!) {
-                if (ship.id == fieldOne[x + 1][y + 1]) {
-                    currentShip = ship
-                    break
-                }
-            }
-
-            if (currentShip != null) {
-                button.setPressedStatus()
-                button.setBackgroundColor(hitColor)
-                currentShip.incrementDestroyedParts()
-                fieldOne[x + 1][y + 1] = -1
-
-                if (currentShip.isDestroyed) {
-                    reflector(currentShip, buttonFieldOne, fieldOne)
-                }
-                updateStep()
-
-                var n = 0
-                for (ship in shipsOne!!) {
-                    if (ship.isDestroyed) n++
-                }
-
-                if (n == 10) handleGameEnd(2)
-            }
-
-            if (currentShip == null) {
-                button.setPressedStatus()
-                button.background = layout!![22]
-                fieldOne[x + 1][y + 1] = -1
-                changeStep()
-
-                if(allButtonsMatchCriteria()) handleGameEnd(2)
-            }
-        }
+        val x = v.id.toString()[1].code - 48
+        val y = v.id.toString()[2].code - 48
+        handleButtonClick(true, x, y)
     }
 
     // Обработка нажатия на кнопку игрового поля второго игрока
     private val secondFieldListener = View.OnClickListener { v ->
-        val button = v as CustomButton
-
-        if (!button.isButtonPressed) {
-            val x = v.getId().toString()[1].code - 48
-            val y = v.getId().toString()[2].code - 48
-            var currentShip: PlayableShip? = null
-
-            for (ship in shipsTwo!!) {
-                if (ship.id == fieldTwo[x + 1][y + 1]) {
-                    currentShip = ship
-                    break
-                }
-            }
-            if (currentShip != null) {
-                button.setPressedStatus()
-                button.setBackgroundColor(hitColor)
-                currentShip.incrementDestroyedParts()
-                fieldTwo[x + 1][y + 1] = -1
-
-                if (currentShip.isDestroyed) {
-                    reflector(currentShip, buttonFieldTwo, fieldTwo)
-                }
-                updateStep()
-
-                var n = 0
-                for (ship in shipsTwo!!) {
-                    if (ship.isDestroyed) n++
-                }
-
-                if (n == 10) handleGameEnd(1)
-            }
-
-            if (currentShip == null) {
-                button.setPressedStatus()
-                button.background = layout!![22]
-                fieldTwo[x + 1][y + 1] = -1
-                changeStep()
-
-                if(allButtonsMatchCriteria()) handleGameEnd(1)
-            }
-        }
+        val x = v.id.toString()[1].code - 48
+        val y = v.id.toString()[2].code - 48
+        handleButtonClick(false, x, y)
     }
 }
